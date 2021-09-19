1141ab0e018002fdcab038587666e1a149953a3f
Pingweiyi <90896464+Pingweiyi@users.noreply.github.com>
2021-09-20 05:06:51 +0800

    Add files via upload

---
+++ t3.js
@@ -0,0 +1,21 @@
+const HelloWorld = Vue.createApp({
+	data(){
+	    return{
+			title:Todos:"",
+			todoLists:[
+				{id:"0", text:"Breakfast", checked: true},
+				{id:"1", text:"Lunch", checked: false},
+				{id:"2", text:"Dinner", checked: false}
+			]
+		 };
+	},	 
+
+    methods:{
+	    toggleItem(todo){
+		    todo.checked =!todo.checked
+	        }
+        }
+
+});
+
+HelloWorld.mount("#APP");
